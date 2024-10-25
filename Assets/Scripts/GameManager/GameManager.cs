fee331f4079e4264cba7890532755999f7a781ae
Luis Jiménez <magnatalesgames@gmail.com>
2024-10-26 00:27:55 +0200

    Add InputCamera and set the default target of ProCamera2D

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,4 +1,5 @@
 using Character;
+using Com.LuisPedroFonseca.ProCamera2D;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
@@ -6,11 +7,15 @@ public class GameManager
 {
     private readonly Hero _character;
     private readonly Camera _gameCamera;
+    private readonly InputCamera _inputCamera;
     
     public GameManager(HeroView heroView, Camera gameCamera)
     {
+        var characterView = Object.Instantiate(heroView);
         _gameCamera = Object.Instantiate(gameCamera);
-        _character = new Hero(Object.Instantiate(heroView), _gameCamera);
+        _character = new Hero(characterView, _gameCamera);
+        _gameCamera.GetComponent<ProCamera2D>().CameraTargets.Add(new CameraTarget(){TargetTransform = characterView.transform});
+        _inputCamera = new InputCamera(_gameCamera);
         GameplayLoopAsync().Forget();
     }
 
@@ -19,6 +24,7 @@ public class GameManager
         while (Application.isPlaying)
         {
             _character.Update();
+            _inputCamera.Update();
             await UniTask.Yield();
         }
     }
